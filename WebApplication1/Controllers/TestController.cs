5858fb671277ad7d64ea3fd0eef91c9990be1e0d
DuyTan <nhoxbybyby142133@gmail.com>
2021-03-24 15:55:28 +0700

    Add project files.

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello World is old now. It’s time for wassup bro ;)";
+        }
+    }
+}
