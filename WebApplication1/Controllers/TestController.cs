fcadc412eac1acf339394b5eff12ec27b88a1269
DuyTan <nhoxbybyby142133@gmail.com>
2021-03-24 17:00:41 +0700

    Add MVC for Employee

--- WebApplication1/Controllers/TestController.cs
+++ WebApplication1/Controllers/TestController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
 {
@@ -14,18 +16,33 @@ namespace WebApplication1.Controllers
 
     public class TestController : Controller
     {
-        public Customer GetCustomer()
-        {
-            Customer c = new Customer();
-            c.CustomerName = "Customer 1";
-            c.Address = "Address 1";
-
-            return c;
-        }
-
         public ActionResult GetView()
         {
-            return View("MyView");
+            EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
+
+            EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
+            List<Employee> employees = empBal.GetEmployees();
+
+            List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
+
+            foreach (Employee emp in employees)
+            {
+                EmployeeViewModel empViewModel = new EmployeeViewModel();
+                empViewModel.EmployeeName = emp.FirstName + " " + emp.LastName;
+                empViewModel.Salary = emp.Salary.ToString("C");
+                if (emp.Salary > 15000)
+                {
+                    empViewModel.SalaryColor = "yellow";
+                }
+                else
+                {
+                    empViewModel.SalaryColor = "green";
+                }
+                empViewModels.Add(empViewModel);
+            }
+            employeeListViewModel.Employees = empViewModels;
+            employeeListViewModel.UserName = "Admin";
+            return View("MyView", employeeListViewModel);
         }
     }
 }
